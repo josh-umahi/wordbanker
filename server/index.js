5710b829deb4f8167d8a7830865eb0a745441c41
josh-umahi <thejoshumahi@gmail.com>
2022-06-11 00:49:31 -0700

    Currently implementing the backend to set up database schema and routes

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config()
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true })) // 30mb limit is because I'll be working with images that may be large in size
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = `mongodb+srv://${process.env.CONNECTION_URL_USERNAME}:${process.env.CONNECTION_URL_PASSWORD}@cluster0.vlpsr4k.mongodb.net/?retryWrites=true&w=majority`
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
