66abe3e5bd3a8782cf657b7996f56e58744848e5
josh-umahi <thejoshumahi@gmail.com>
2022-07-14 20:20:05 -0700

    Minor changes, implemented 404 page

--- client/src/routes/Home/styles.js
+++ client/src/routes/Home/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(theme => ({
+export default makeStyles(() => ({
     postsListing: {
         backgroundColor: "#f6f5f5",
         padding: "2em 0 1em",
