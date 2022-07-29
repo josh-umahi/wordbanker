552492aeecdc6a540b90503d22f030327363f600
NG-Joseph <jcool4liv@gmail.com>
2022-07-29 20:12:39 +0100

    minor fix in appcontext.tsx

--- client/src/context/AppContext.tsx
+++ client/src/context/AppContext.tsx
@@ -1,8 +1,9 @@
 import React, { createContext, PropsWithChildren, useContext, useState } from "react";
 import { User } from "../../types/User";
+import { getLocalStorageProfile } from "../utils/localStorage";
 
 type AppContextType = {
-  user: User;
+  user: User| null;
   //React.Dispatch<React.SetStateAction<User>>??
   handleSetUser: (user: User | null) => void;
 } | null;
@@ -13,7 +14,7 @@ export const useAppContext = () => useContext(AppContext);
 // React.FC<PropsWithChildren> = React.FunctionComponent
 // Using so we can access the children prop without any TS complaints
 export const AppContextProvider: React.FC<PropsWithChildren> = ({ children }) => {
-  const initialState = JSON.parse(localStorage.getItem("profile") || "{}"); // Set to empty object if no profile is found
+  const initialState = getLocalStorageProfile() as User | null;
   const [user, setUser] = useState(initialState);
 
   const handleSetUser = (value: User | null) => setUser(value);
